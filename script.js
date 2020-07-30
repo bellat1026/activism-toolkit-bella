78532956f3b80e71e0f3c28cb45dd1134b080554
Glitch (activist-toolkit-bella) <none>
2020-07-30 12:33:13 +0000

    🛳⛲️ Checkpoint ./script.js:19022667/1321 ./style.css:19022667/145

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 /********* ADD CHECKLIST FUNCTIONALITY **********/
 /************************************************/
 /* TODO: Add a variable to store the "my-list" element */
-var fullItemList = document.getElementById("my-list”);
+var fullItemList = document.getElementById("my-list");
 
 /* TODO: Create the event listener that listens for a mouse click and runs the checkOffList function */
 if (fullItemList) {
@@ -11,8 +11,7 @@ if (fullItemList) {
 
 /* TODO: Declare the function checkOffList and add actions inside the { } */
 function checkOffItem(clicked) {
-    if (clicked.target.tagName == "LI") {
-        clicked.target.classList.toggle("all-done");
-    }
+  if (clicked.target.tagName == "LI") {
+    clicked.target.classList.toggle("all-done");
+  }
 }
-
